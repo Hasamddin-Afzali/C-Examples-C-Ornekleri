d10751b1833102572d53cbf6c642d31de43fb8c3
Hasamddin-Afzali <66642113+Hasamddin-Afzali@users.noreply.github.com>
2023-11-29 14:16:56 +0300

    Add files via upload

---
+++ WordCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Words : ");
+            //Console.WriteLine("Not * ' ', ',', '.', '-', '\n', '\t'");
+            string word = Console.ReadLine();
+            string[] wordCounts = word.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            foreach(string newWords in wordCounts)
+            {
+                Console.WriteLine($"{newWords}");
+            }
+            Console.WriteLine($"Total Word Count is : {wordCounts.Length}");
+            Console.ReadKey();
+        }
+    }
+}
