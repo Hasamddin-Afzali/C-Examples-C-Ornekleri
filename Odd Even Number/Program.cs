56e2f31958ea74e27ead2bb24d5d76b37c268bbf
Hasamddin-Afzali <66642113+Hasamddin-Afzali@users.noreply.github.com>
2023-12-01 22:40:36 +0300

    Add files via upload

---
+++ Odd Even Number/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Number : ");
+            
+            bool isNumber = false;
+            int number = 0;
+
+            while (!isNumber)
+            {
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    isNumber = true;
+                }catch (FormatException)
+                {
+                    isNumber = false;
+                    Console.WriteLine("Please Enter A number !");
+                }
+            }
+            if(number %2 == 0)
+            {
+                Console.WriteLine("The Number is Even !");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Even Odd !");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
