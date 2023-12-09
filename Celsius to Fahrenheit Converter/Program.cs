4f0a635049d08b3d5e0e7e35478ccea1d8702d81
Hasamddin-Afzali <66642113+Hasamddin-Afzali@users.noreply.github.com>
2023-12-09 17:06:24 +0300

    Add files via upload

---
+++ Celsius to Fahrenheit Converter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celsius_to_Fahrenheit_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isNumber = false;
+            float Celsius = 0, result;
+            Console.WriteLine("Please enter the Celsius : ");
+            
+            while (!isNumber) 
+            {
+                try
+                {
+                    Celsius = float.Parse(Console.ReadLine());
+                    isNumber = true;
+                }catch (FormatException)
+                {
+                    isNumber = false;
+                    Console.WriteLine("Please enter a number ");
+                }
+            }
+            result = (Celsius * 9 / 5) + 32;
+            Console.WriteLine($"{Celsius} Celsius is {result} Fahrenheit");
+            Console.ReadKey();
+        }
+    }
+}
